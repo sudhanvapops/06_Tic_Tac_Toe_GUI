0644be2285f5034eabb3ec924ff4a9c49be1ff66
Sudhanva <sudhanvapops@gmail.com>
2024-07-30 13:37:26 +0530

    Button Class Changed for Pixel Perfect Collision

--- Button_Class.py
+++ Button_Class.py
@@ -1,31 +1,33 @@
 import pygame
 
-# Class
 class Button():
     def __init__(self, x, y, image, scale=1):
         width = image.get_width()
         height = image.get_height()
-        self.image = pygame.transform.scale(image, (int(width*scale), int(height*scale)))
+        self.image = pygame.transform.scale(image, (int(width * scale), int(height * scale)))
         self.rect = self.image.get_rect()
-        # self.mask_image = pygame.mask.from_surface(self.image)
-        self.rect.center = (x,y)
+        self.rect.center = (x, y)
+        self.mask = pygame.mask.from_surface(self.image)  # Create mask from the button image
         self.clicked = False
-    
-    def draw(self,surface):
+
+    def draw(self, surface):
         action = False
         pos = pygame.mouse.get_pos()
 
         # Check mouse clicked and mouse hover
-        if pygame.mouse.get_pressed()[0] == 1 and self.clicked == False:
-            # check for colllison
+        if pygame.mouse.get_pressed()[0] == 1 and not self.clicked:
+            # Check for collision with pixel-perfect detection
             if self.rect.collidepoint(pos):
-                self.clicked = True
-                action = True
-        
-        if pygame.mouse.get_pressed()[0] == 0 and self.clicked == True:
+                mouse_x, mouse_y = pos
+                mouse_pos = (mouse_x - self.rect.left, mouse_y - self.rect.top)
+                if 0 <= mouse_pos[0] < self.mask.get_size()[0] and 0 <= mouse_pos[1] < self.mask.get_size()[1]:
+                    if self.mask.get_at(mouse_pos):
+                        self.clicked = True
+                        action = True
+
+        if pygame.mouse.get_pressed()[0] == 0 and self.clicked:
             self.clicked = False
 
         surface.blit(self.image, self.rect)
 
         return action
-    
