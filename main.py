cc90d78a91ff6bd03a6b12ed97dcd0fde2b8b683
Sudhanva <sudhanvapops@gmail.com>
2024-07-31 08:57:38 +0530

    Deleted test.py

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import pygame
 from sys import exit
-import main_menu
 
 
 pygame.init()
@@ -25,50 +24,45 @@ def game_play(screen):
 
 
     while True:
-        if variables.game_state == "main_menu":
 
-            pass
-        
-        elif variables.game_state == "game":
+        screen.fill(variables.COLOR)
 
-            screen.fill(variables.COLOR)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                exit()
 
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    pygame.quit()
-                    exit()
+            if event.type == pygame.MOUSEBUTTONDOWN and pygame.mouse.get_pressed()[0] == True and variables.game_over == False:
 
-                if event.type == pygame.MOUSEBUTTONDOWN and pygame.mouse.get_pressed()[0] == True and variables.game_over == False:
+                # To ge Mouse Position
+                mouse_pos = pygame.mouse.get_pos()
 
-                    # To ge Mouse Position
-                    mouse_pos = pygame.mouse.get_pos()
+                # X is Row , Y is Column  returns tupule of index (x,y)
+                x = round((mouse_pos[1] / 100)  )  - 1
+                y = round((mouse_pos[0] / 100)  )  - 1
 
-                    # X is Row , Y is Column  returns tupule of index (x,y)
-                    x = round((mouse_pos[1] / 100)  )  - 1
-                    y = round((mouse_pos[0] / 100)  )  - 1
+                functions.change_X(variables.X_State,x,y)
 
-                    functions.change_X(variables.X_State,x,y)
-
-                # For Restart Game
-                if (event.type == pygame.KEYDOWN and event.key == pygame.K_r):
-                    variables.game_over = False
-                    variables.Turn = True
-                    variables.Draw = False
-                    variables.wining_line = ()
-                    variables.X_State = [[False,False,False],[False,False,False],[False,False,False]]
-                    variables.O_State = [[False,False,False],[False,False,False],[False,False,False]]
+            # For Restart Game
+            if (event.type == pygame.KEYDOWN and event.key == pygame.K_r):
+                variables.game_over = False
+                variables.Turn = True
+                variables.Draw = False
+                variables.wining_line = ()
+                variables.X_State = [[False,False,False],[False,False,False],[False,False,False]]
+                variables.O_State = [[False,False,False],[False,False,False],[False,False,False]]
 
 
-            screen.blit(variables.Board,variables.Board_Rect)
-            functions.draw_board(variables.O_State,screen)
-            if variables.game_over == True:
-                if variables.Draw == True:
-                    functions.draw_text(screen,variables.win_text)
-                else:
-                    functions.draw_text(screen,variables.win_text)
-                    functions.draw_line(screen,variables.wining_line[0],variables.wining_line[1])
+        screen.blit(variables.Board,variables.Board_Rect)
+        functions.draw_board(variables.O_State,screen)
+        if variables.game_over == True:
+            if variables.Draw == True:
+                functions.draw_text(screen,variables.win_text)
+            else:
+                functions.draw_text(screen,variables.win_text)
+                functions.draw_line(screen,variables.wining_line[0],variables.wining_line[1])
 
-            pygame.display.update()
+        pygame.display.update()
 
         clock.tick(60)
 
